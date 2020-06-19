bb0322634443b75bfec1af5bf4a7673a49c79266
aavezel <aavezel@gmail.com>
2020-06-19 17:51:17 +0300

    clear. add body parser

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
 const apiRouter = require('./routes/api');
@@ -9,12 +9,15 @@ const apiRouter = require('./routes/api');
 const app = express();
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
+
+
 
 module.exports = app;
