02a73e386ad4a0c08421199605c114eb34a43a02
aavezel <aavezel@gmail.com>
2020-06-22 09:16:27 +0300

    выносим контроллеры

---
+++ server/controllers/fileController.js
@@ -0,0 +1,41 @@
+async function getAllFiles(req, res, next) {
+    const allFiles = await repository.getRepository().getAllFiles();
+    res.json(allFiles);
+}
+
+async function getFileInfo(req, res, next) {
+    var file_id = req.params.file_id;
+    const file = await repository.getRepository().getFileById(file_id);
+    res.json(file);
+}
+
+async function addFile({ body: { title = null } } = {}, res, next) {
+    if (title === null) {
+        next("title is empty");
+        return;
+    }
+    const allFiles = await repository.getRepository().addFile(title);
+    res.json(allFiles);
+}
+
+async function deleteFile(req, res, next) {
+    var file_id = req.params.file_id;
+    await repository.getRepository().deleteFile(file_id);
+    res.json({ "status": "ok" });
+}
+
+async function uploadFile(req, res, next) {
+    // TODO: add catch
+    var file_id = req.params.file_id;
+    const { originalname, filename } = req.file;
+    await repository.getRepository().uploadFile(file_id, filename, originalname);
+    res.json({ "status": "ok" });
+}
+
+module.exports = {
+    getAllFiles,
+    getFileInfo,
+    addFile,
+    deleteFile,
+    uploadFile,
+}
--- server/routes/api.js
+++ server/routes/api.js
@@ -3,10 +3,10 @@ const express = require('express');
 const router = express.Router();
 
 const config = require("../config");
-const os = require("os");
 const multer  = require('multer');
 const upload = multer({dest: config.upload_file_path, limits: {fileSize: 1024*1024, files: 1 } });
 
+const { getAllFiles, getFileInfo, addFile, deleteFile, uploadFile } = require("../controllers/fileController");
 
 
 router.route('/')
@@ -28,42 +28,6 @@ module.exports = router;
 
 
 /*-------------------------*/
-// TODO : controllers?
-
-async function getAllFiles(req, res, next) {
-  const allFiles = await repository.getRepository().getAllFiles();
-  res.json(allFiles);
-}
-
-async function getFileInfo(req, res, next) {
-  var file_id = req.params.file_id;
-  const file = await repository.getRepository().getFileById(file_id);
-  res.json(file);
-}
-
-async function addFile({body: {title = null}} = {}, res, next) {  
-  if (title === null) {
-    next("title is empty");
-    return;
-  }
-  const allFiles = await repository.getRepository().addFile(title);
-  res.json(allFiles);
-}
-
-async function deleteFile(req, res, next) {
-  var file_id = req.params.file_id;
-  await repository.getRepository().deleteFile(file_id);
-  res.json({"status": "ok"});
-}
-
-async function uploadFile(req, res, next) {  
-  // TODO: add catch
-  var file_id = req.params.file_id;
-  const {originalname, filename} = req.file;
-  await repository.getRepository().uploadFile(file_id, filename, originalname);
-  res.json({"status": "ok"});
-}
-
 function error404(req, res, next) {
   res.status(404).send({ 'error': 'method not found' });
   next();
