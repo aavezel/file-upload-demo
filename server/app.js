2f472271867590ea0a9584a2ad5f93edfde329d3
aavezel <aavezel@gmail.com>
2020-06-25 14:27:36 +0300

    Добавил cors для разработки

--- server/app.js
+++ server/app.js
@@ -7,6 +7,8 @@ const indexRouter = require('./routes/index');
 const apiRouter = require('./routes/api');
 const authRouter = require('./routes/auth');
 const getFileRouter = require("./routes/file");
+const cors = require('cors')
+
 
 const app = express();
 
@@ -16,6 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(cookieParser());
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/auth', authRouter);
